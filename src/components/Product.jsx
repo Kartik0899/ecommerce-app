8bcbbbc3d46a84ca59d2d595fd0e2a400b323ca1
Kartik0899 <jaybudhraja08@gmail.com>
2023-01-16 00:15:34 +0530

    Cart and Checkout update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink, useParams } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
+import { useDispatch } from 'react-redux';
+import { addCart } from '../redux/action';
 
 const Product = () => {
 
     const { id } = useParams();
     const [product, setProduct] = useState([])
     const [loading, setLoading] = useState(false);
-    console.log(product);
+
+    const dispatch = useDispatch();
+    const addProduct = (product) => {
+        dispatch(addCart(product));
+    }
+
 
     useEffect(() => {
         const getProduct = async () => {
@@ -63,7 +70,7 @@ const Product = () => {
                         ${product.price}
                     </h3>
                     <p className="lead">{product.description}</p>
-                    <button className="btn btn-outline-dark px-4 py-2">Add to Cart</button>
+                    <button className="btn btn-outline-dark px-4 py-2" onClick={() => addProduct(product)}>Add to Cart</button>
                     <NavLink to='/cart' className='btn btn-dark ms-2 px-3 py-2'>Go to Cart</NavLink>
                 </div>
             </>
